6a046417152b4d9e8ffa4d6eea81f27b413f4dd0
Lohitha Garapati <143539852+Lohitha263@users.noreply.github.com>
2025-08-11 22:27:54 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://authentication_owner:npg_RPzlg90otuvJ@ep-young-dust-a4g6zpin-pooler.us-east-1.aws.neon.tech/authentication?sslmode=require&channel_binding=require',
+      url: 'your postgre sql link',
     }
   };
